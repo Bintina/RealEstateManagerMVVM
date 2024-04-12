c34d0e43bd3d32bab34cb9dc3d3021eb784cd7ad
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-12 13:26:13 +0300

    Updated res files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.bintina.realestatemanagermvvm"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.bintina.realestatemanagermvvm"
---
+++ app/src/main/java/com/bintina/realestatemanagermvvm/TempSpace.kt
@@ -0,0 +1,28 @@
+package com.bintina.realestatemanagermvvm
+
+class TempSpace {
+/*//Strings
+    <string name="app_name">Real Estate Manager MVVM</string>*/
+
+/*    //Themes
+    <resources xmlns:tools="http://schemas.android.com/tools">
+    <!-- Base application theme. -->
+    <style name="Base.Theme.RealEstateManagerMVVM" parent="Theme.Material3.DayNight.NoActionBar">
+    <!-- Customize your light theme here. -->
+    <!-- <item name="colorPrimary">@color/my_light_primary</item> -->
+    </style>
+
+    <style name="Theme.RealEstateManagerMVVM" parent="Base.Theme.RealEstateManagerMVVM" />
+    </resources>*/
+
+
+/*    //Themes Night
+    <resources xmlns:tools="http://schemas.android.com/tools">
+    <!-- Base application theme. -->
+    <style name="Base.Theme.RealEstateManagerMVVM" parent="Theme.Material3.DayNight.NoActionBar">
+    <!-- Customize your dark theme here. -->
+    <!-- <item name="colorPrimary">@color/my_dark_primary</item> -->
+    </style>
+    </resources>*/
+
+}
