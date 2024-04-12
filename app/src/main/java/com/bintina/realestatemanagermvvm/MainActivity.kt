224ece4133aa8cac1112fb5fdcf0d0ea9594713c
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-12 13:39:03 +0300

    The two onset changes as described in the the Project description

--- app/src/main/java/com/bintina/realestatemanagermvvm/MainActivity.kt
+++ app/src/main/java/com/bintina/realestatemanagermvvm/MainActivity.kt
@@ -2,10 +2,15 @@ package com.bintina.realestatemanagermvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val welcomeTextView = findViewById<TextView>(R.id.home_text_view)
+
+        welcomeTextView.text = "Le premier immobilier enregistre vaut 81"
     }
 }
