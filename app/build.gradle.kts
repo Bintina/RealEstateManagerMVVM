7c3fdaa1b21925c47daaf45dc733c280d1430d10
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-15 11:55:34 +0300

    Gradle conflict resolved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,13 +45,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.annotation:annotation:1.7.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-
-    //VIEW MODEL & LIVE DATA
-    implementation("android.arch.lifecycle:extensions:1.1.1")
 
     // Navigation Component
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
@@ -64,6 +62,7 @@ dependencies {
     androidTestImplementation("androidx.room:room-testing:2.6.1")
 
     // Lifecycle components
+
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
