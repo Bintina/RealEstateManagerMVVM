4a77ba001c86e70c4b769b3e169c2bb7fb6c4bdb
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-20 12:17:21 +0300

    Real Estate list Fragment configured and linked to Adapter

--- app/src/main/java/com/bintina/realestatemanagermvvm/realestate/view/ListPropertiesFragment.kt
+++ app/src/main/java/com/bintina/realestatemanagermvvm/realestate/view/ListPropertiesFragment.kt
@@ -1,4 +1,39 @@
 package com.bintina.realestatemanagermvvm.realestate.view
 
-class ListPropertiesFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bintina.realestatemanagermvvm.databinding.FragmentListPropertiesBinding
+import com.bintina.realestatemanagermvvm.realestate.view.adapter.Adapter
+
+class ListPropertiesFragment(): Fragment() {
+
+    lateinit var adapter: com.bintina.realestatemanagermvvm.realestate.view.adapter.Adapter
+
+    private var _binding: FragmentListPropertiesBinding? = null
+    private val binding get()= _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentListPropertiesBinding.inflate(inflater, container, false)
+
+        initializeViews()
+
+        return binding.root
+    }
+
+    private fun initializeViews() {
+        adapter = Adapter()
+        binding.propertiesListRecyclerContainer.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
