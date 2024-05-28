efb49844aa911eb3aac4d96bb8647b5268121915
Bintina <105106238+Bintina@users.noreply.github.com>
2024-05-28 19:09:02 +0300

    Red. Toolbar test

---
+++ app/src/androidTest/java/com/bintina/realestatemanagermvvm/MainInstrumentedTests.kt
@@ -0,0 +1,22 @@
+package com.bintina.realestatemanagermvvm
+
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertNotNull
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainInstrumentedTests {
+
+    @Rule
+    @JvmField
+    val rule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun toolbar_instantiated(){
+        assertNotNull(withId(R.id.toolbar))
+    }
+}
