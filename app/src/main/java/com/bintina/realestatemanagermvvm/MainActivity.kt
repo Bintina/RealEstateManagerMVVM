5407ba5a03ce660e990b7110ca428c528c6a840e
Bintina <105106238+Bintina@users.noreply.github.com>
2024-06-01 09:59:53 +0300

    Initial login view.xml and binding set up. Manifest internet permissions granted.

--- app/src/main/java/com/bintina/realestatemanagermvvm/MainActivity.kt
+++ app/src/main/java/com/bintina/realestatemanagermvvm/MainActivity.kt
@@ -31,7 +31,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        getSha()
 
         val toolbar = binding.toolbar
         setSupportActionBar(toolbar)
@@ -76,49 +75,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-     @RequiresApi(Build.VERSION_CODES.P)
- fun getSha() {
-     val packageName = "com.bintina.realestatemanagermvvm"
 
-     try {
-         val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
-
-         for (signature in info.signingInfo.signingCertificateHistory) {
-
-             // Compute SHA-1 fingerprint
-             val sha1 = MessageDigest.getInstance("SHA-1")
-             sha1.update(signature.toByteArray())
-             val sha1Hex = bytesToHex(sha1.digest())
-             Log.d("SHA-1", sha1Hex)
-
-             //SHA-256
-             val md: MessageDigest = MessageDigest.getInstance("SHA-256")
-             md.update(signature.toByteArray())
-
-             // Convert the byte array to hexadecimal representation
-             val sha256Hex = bytesToHex(md.digest())
-
-             Log.d("SHA", sha256Hex)
-
-             val keyHash = String(Base64.encode(md.digest(), Base64.NO_WRAP))
-             Log.e("hash key", keyHash)
-         }
-     } catch (e: PackageManager.NameNotFoundException) {
-         Log.e("name not found", e.toString())
-     } catch (e: NoSuchAlgorithmException) {
-         Log.e("no such algorithm", e.toString())
-     } catch (e: Exception) {
-         Log.e("exception", e.toString())
-     }
- }
-
- private fun bytesToHex(bytes: ByteArray): String {
-     val hexChars = CharArray(bytes.size * 2)
-     for (i in bytes.indices) {
-         val v = bytes[i].toInt() and 0xFF
-         hexChars[i * 2] = "0123456789ABCDEF"[v shr 4]
-         hexChars[i * 2 + 1] = "0123456789ABCDEF"[v and 0x0F]
-     }
-     return String(hexChars)
- }
 }
--- app/src/main/java/com/bintina/realestatemanagermvvm/TempSpace.kt
+++ app/src/main/java/com/bintina/realestatemanagermvvm/TempSpace.kt
@@ -1,5 +1,13 @@
 package com.bintina.realestatemanagermvvm
 
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Base64
+import android.util.Log
+import androidx.annotation.RequiresApi
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 class TempSpace {
 /*//Strings
     <string name="app_name">Real Estate Manager MVVM</string>*/
@@ -25,4 +33,50 @@ class TempSpace {
     </style>
     </resources>*/
 
+
+/*    @RequiresApi(Build.VERSION_CODES.P)
+    fun getSha() {
+        val packageName = "com.bintina.realestatemanagermvvm"
+
+        try {
+            val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+
+            for (signature in info.signingInfo.signingCertificateHistory) {
+
+                // Compute SHA-1 fingerprint
+                val sha1 = MessageDigest.getInstance("SHA-1")
+                sha1.update(signature.toByteArray())
+                val sha1Hex = bytesToHex(sha1.digest())
+                Log.d("SHA-1", sha1Hex)
+
+                //SHA-256
+                val md: MessageDigest = MessageDigest.getInstance("SHA-256")
+                md.update(signature.toByteArray())
+
+                // Convert the byte array to hexadecimal representation
+                val sha256Hex = bytesToHex(md.digest())
+
+                Log.d("SHA", sha256Hex)
+
+                val keyHash = String(Base64.encode(md.digest(), Base64.NO_WRAP))
+                Log.e("hash key", keyHash)
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+            Log.e("name not found", e.toString())
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e("no such algorithm", e.toString())
+        } catch (e: Exception) {
+            Log.e("exception", e.toString())
+        }
+    }
+
+    private fun bytesToHex(bytes: ByteArray): String {
+        val hexChars = CharArray(bytes.size * 2)
+        for (i in bytes.indices) {
+            val v = bytes[i].toInt() and 0xFF
+            hexChars[i * 2] = "0123456789ABCDEF"[v shr 4]
+            hexChars[i * 2 + 1] = "0123456789ABCDEF"[v and 0x0F]
+        }
+        return String(hexChars)
+    }*/
 }
