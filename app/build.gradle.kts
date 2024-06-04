7dea4edc56c8a7da496cd2bab0489f5bc942bc17
Bintina <105106238+Bintina@users.noreply.github.com>
2024-06-04 12:21:55 +0300

    Build completed without wanings.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,13 +94,11 @@ dependencies {
     //Testing
     implementation("androidx.test:core-ktx:1.5.0")
     implementation("androidx.test:rules:1.5.0")
-    implementation("androidx.test.expresso:espresso-contrib:3.5.1")
+    implementation("androidx.test.espresso:espresso-contrib:3.5.1")
     testImplementation("androidx.test:core-ktx:1.5.0")
     testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.test:runner:1.5.2")
-    testImplementation("androidx.test:rules.1.5.0")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
-    testImplementation("org.jetbrains.kotlinx.kotlinx-coroutines-test:1.8.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
     debugImplementation("androidx.fragment:fragment-testing:1.7.1")
@@ -116,7 +114,7 @@ dependencies {
     // Espresso for UI testing (optional, if you need it)
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.test.espresso:espresso-contrib:3.5.1")
+    //implementation("androidx.test.espresso:espresso-contrib:3.5.1")
     //testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.7.7")
 
 
